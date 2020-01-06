c2bf2a17674bf6761c3c9d73b5dba2d28dee2a35
madhu <madhu.constacloud@gmail.com>
2020-01-06 12:41:59 +0530

    check http guzzle request

--- src/Http/controllers/BigcommerceController.php
+++ src/Http/controllers/BigcommerceController.php
@@ -4,7 +4,8 @@ namespace Bigcommerce\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
 
 class BigcommerceController extends Controller {
@@ -14,7 +15,14 @@ class BigcommerceController extends Controller {
        print_r("hello");die;
     }
 
-   
+    public function getCatApi()
+    {
+        $client = new Client(); //GuzzleHttp\Client
+        $request = $client->get('https://api.thecatapi.com/v1/breeds');
+        $response = $request->getBody();
+       
+        dd($response);
+    }
 
 
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,4 +2,5 @@
 <?php
 Route::group(['namespace' => 'Bigcommerce\Http\Controllers', 'middleware' => ['web']], function(){
     Route::get('showtest', 'BigcommerceController@showText');
+    Route::get('getcatapi', 'BigcommerceController@getCatApi');
 });
